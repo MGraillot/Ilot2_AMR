1447db4c173612ca19ded1cdd36b4ad14c3dca6a
Mathieu Graillot <mathieu.graillot@ynov.com>
2024-03-31 21:02:08 +0200

    Motor 1/2/3/4 with run/stop... & L298N

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,116 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* // TEST SENSOR IR TELECOMMANDE + COMMAND WITH AMR
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define PIN_IR 17
+#define START_AMR 0xFFA25D     // Button ON-OFF
+#define STOP_AMR 0xFFE21D      // Button MENU
+#define PIN_SERVO01 16         // Servo scan
+#define PIN_DIRECTION_RIGHT 15 // Motor
+
+Servo SG90Servo;
+IRrecv irrecv(PIN_IR);
+decode_results results;
+
+void setup()
+{
+  irrecv.enableIRIn();
+  Serial.begin(9600);
+
+  SG90Servo.attach(PIN_SERVO01);
+  pinMode(PIN_DIRECTION_RIGHT, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // ESSAIS IR REMOTE START/STOP ACTION
+
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value, HEX);
+    if (results.value == START_AMR)
+    {
+      SG90Servo.write(150);
+      delay(10);
+    }
+    else if (results.value == STOP_AMR)
+    {
+      SG90Servo.write(0);
+      delay(10);
+    }
+    delay(1000);
+    irrecv.resume();
+  }
+}*/
+
+// TEST MOTEURS - MOTOREDUCTEURS x2
+
+// Motor 1
+#define IN1 18
+#define IN2 5
+// Motor 2
+#define IN3 17
+#define IN4 16
+
+void setup()
+{
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void run()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void stop()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void move_back()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void trun_right()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void trun_left()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void loop()
+{
+  run();
+  delay(1000);
+  stop();
+  delay(2500);
+  trun_right();
+  delay(5000);
+  move_back();
+  delay(3000);
 }
